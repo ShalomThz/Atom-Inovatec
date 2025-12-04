b4e51347756cad2387c3a32a8db61fec4d479d26
Shalom <tjoelshalom@gmail.com>
2025-12-03 23:17:27 -0600

    feature task calendar

---
+++ resources/views/filament/pages/calendar-board.blade.php
@@ -0,0 +1,17 @@
+<x-filament-panels::page>
+    <div id="calendar-view-root"
+         data-tareas="{{ json_encode($tareas) }}"
+         class="calendar-view-container">
+        <div class="flex items-center justify-center p-8">
+            <div class="text-gray-500">Cargando calendario...</div>
+        </div>
+    </div>
+
+    @push('scripts')
+    @vite('resources/js/calendar-init.tsx')
+    @endpush
+
+    @push('styles')
+    @vite('resources/css/calendar.css')
+    @endpush
+</x-filament-panels::page>
